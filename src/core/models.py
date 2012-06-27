d83a48aa834af3ac9b158ef27d7d4c204d32a7c4
Flávio França <flaviolrf@gmail.com>
2012-06-26 22:10:56 -0300

    deleted:    src/__init__.py 	deleted:    src/core/__init__.py 	deleted:    src/core/models.py 	deleted:    src/core/tests.py 	deleted:    src/core/views.py 	deleted:    src/public/admin/css/base.css 	deleted:    src/public/admin/css/changelists.css 	deleted:    src/public/admin/css/dashboard.css 	deleted:    src/public/admin/css/forms.css 	deleted:    src/public/admin/css/ie.css 	deleted:    src/public/admin/css/login.css 	deleted:    src/public/admin/css/rtl.css 	deleted:    src/public/admin/css/widgets.css 	deleted:    src/public/admin/img/changelist-bg.gif 	deleted:    src/public/admin/img/changelist-bg_rtl.gif 	deleted:    src/public/admin/img/chooser-bg.gif 	deleted:    src/public/admin/img/chooser_stacked-bg.gif 	deleted:    src/public/admin/img/default-bg-reverse.gif 	deleted:    src/public/admin/img/default-bg.gif 	deleted:    src/public/admin/img/deleted-overlay.gif 	deleted:    src/public/admin/img/gis/move_vertex_off.png 	deleted:    src/public/admin/img/gis/move_vertex_on.png 	deleted:    src/public/admin/img/icon-no.gif 	deleted:    src/public/admin/img/icon-unknown.gif 	deleted:    src/public/admin/img/icon-yes.gif 	deleted:    src/public/admin/img/icon_addlink.gif 	deleted:    src/public/admin/img/icon_alert.gif 	deleted:    src/public/admin/img/icon_calendar.gif 	deleted:    src/public/admin/img/icon_changelink.gif 	deleted:    src/public/admin/img/icon_clock.gif 	deleted:    src/public/admin/img/icon_deletelink.gif 	deleted:    src/public/admin/img/icon_error.gif 	deleted:    src/public/admin/img/icon_searchbox.png 	deleted:    src/public/admin/img/icon_success.gif 	deleted:    src/public/admin/img/inline-delete-8bit.png 	deleted:    src/public/admin/img/inline-delete.png 	deleted:    src/public/admin/img/inline-restore-8bit.png 	deleted:    src/public/admin/img/inline-restore.png 	deleted:    src/public/admin/img/inline-splitter-bg.gif 	deleted:    src/public/admin/img/nav-bg-grabber.gif 	deleted:    src/public/admin/img/nav-bg-reverse.gif 	deleted:    src/public/admin/img/nav-bg-selected.gif 	deleted:    src/public/admin/img/nav-bg.gif

--- src/core/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-from django.contrib.localflavor.br.forms import BRCPFField
-
-# Create your models here.
--- src/subscriptions/urls.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-from django.conf.urls import patterns, include, url
-
-
-urlpatterns = patterns('src.subscriptions.views',
-    url(r'^$', 'subscribe', name='subscribe'),
-    url(r'^(\d+)/', 'success', name='success'),
-)
